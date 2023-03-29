f57a62ef1dec2c6db533cd5caf99cf8f6a1761aa
Samuel_Alvarez <s.alvarez05@ufromail.cl>
2023-03-29 09:51:30 -0300

    Actualización inicial codigo

--- Desarrollo/src/main/java/org/example/Main.java
+++ Desarrollo/src/main/java/org/example/Main.java
@@ -5,6 +5,28 @@ import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int dimension;
+        int primes=0;
+        dimension= readDim();
+        int [] vector;
+        vector = new int [dimension];
+        llenarVector(vector);
+        mostrarVector(vector);
+        esPrimo();
+    }
+
+    private static void esPrimo() {
+    }
+
+    private static void mostrarVector(int[] vector) {
+    }
+
+    private static void llenarVector(int[] vector) {
+    }
+
+    private static int readDim() {
+        Scanner teclado = new Scanner(System.in);
+        int dimension= teclado.nextInt();
+        return dimension;
     }
 }
